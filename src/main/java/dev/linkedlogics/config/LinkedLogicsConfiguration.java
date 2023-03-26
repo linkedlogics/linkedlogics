3f75248dd512317e6e9f889716f15b98e935e6ab
radjab <radjab@gmail.com>
2023-03-26 09:53:03 +0200

    yaml parsing lib is changed due to conflict with scala.Map

--- src/main/java/dev/linkedlogics/config/LinkedLogicsConfiguration.java
+++ src/main/java/dev/linkedlogics/config/LinkedLogicsConfiguration.java
@@ -4,15 +4,14 @@ import java.io.InputStream;
 import java.util.Map;
 import java.util.Optional;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.yaml.snakeyaml.Yaml;
 
 public class LinkedLogicsConfiguration {
 	public static final String LINKEDLOGICS = "linkedlogics";
 	public static final String APPLICATION_NAME = "application.name";
 	private static final String CONFIG_FILE = "linkedlogics.yaml";
 	static Map<String, Object> configuration;
-	
+
 	static {
 		try {
 			configuration = load(CONFIG_FILE);
@@ -20,7 +19,7 @@ public class LinkedLogicsConfiguration {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	static Map<String, Object> load(String resource) throws Exception {
 		InputStream input = LinkedLogicsConfiguration.class.getClassLoader().getResourceAsStream(resource);
 		if (input == null) {
@@ -28,33 +27,32 @@ public class LinkedLogicsConfiguration {
 		}
 		return load(input);
 	}
-	
+
 	static Map<String, Object> load(InputStream input) throws Exception {
-		ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-		mapper.findAndRegisterModules();
-		return mapper.readValue(input, Map.class);
+		Yaml yaml = new Yaml();
+		return yaml.load(input);
 	}
-	
+
 	public static boolean containsConfig(String key) {
 		return getConfig(key).isPresent();
 	}
-	
+
 	public static Optional<Object> getConfig(String key) {
 		return getConfig(LINKEDLOGICS, key);
 	}
-	
+
 	public static Optional<Object> getConfig(String prefix, String key) {
 		return Optional.ofNullable(findConfig(String.join(".", prefix, key), configuration));
 	}
-	
+
 	public static Object getConfigOrDefault(String key, Object defaultValue) {
 		return getConfigOrDefault(LINKEDLOGICS, key, defaultValue);
 	}
-	
+
 	public static Object getConfigOrDefault(String prefix, String key, Object defaultValue) {
 		return getConfig(prefix, key).orElse(defaultValue);
 	}
-	
+
 	static Object findConfig(String key, Map<String, Object> configuration) {
 		String[] keys = key.split("\\.");
 		Map<String, Object> cursor = configuration;
@@ -64,7 +62,7 @@ public class LinkedLogicsConfiguration {
 				return null;
 			}
 		}
-		
+
 		return cursor.get(keys[keys.length - 1]);
 	}
 }
