4c3cc8a3e94289e9c60e388258dee2e44cb63d86
radjab <radjab@gmail.com>
2023-04-14 02:33:28 +0200

    queue based callback service impl

--- src/main/java/dev/linkedlogics/service/local/LocalQueueService.java
+++ src/main/java/dev/linkedlogics/service/local/LocalQueueService.java
@@ -2,30 +2,42 @@ package dev.linkedlogics.service.local;
 
 import java.util.Optional;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 
+import dev.linkedlogics.LinkedLogics;
 import dev.linkedlogics.config.LinkedLogicsConfiguration;
 import dev.linkedlogics.service.QueueService;
 
 public class LocalQueueService implements QueueService {
-
-	private ArrayBlockingQueue<String> queue;
+	private ConcurrentHashMap<String, ArrayBlockingQueue<String>> queueMap;
+	private int queueSize;
 	
 	public LocalQueueService() {
-		int queueSize = (Integer) LinkedLogicsConfiguration.getConfigOrDefault("services.consumer.queue-size", 1000);
-		queue = new ArrayBlockingQueue<>(queueSize);
+		queueMap = new ConcurrentHashMap<>();
+		queueSize = (Integer) LinkedLogicsConfiguration.getConfigOrDefault("services.consumer.queue-size", 1000);
+		queueMap.put(LinkedLogics.getApplicationName(), new ArrayBlockingQueue<>(queueSize));
 	}
 	
 	@Override
 	public void offer(String queue, String payload) {
-		this.queue.offer(payload);
+		ArrayBlockingQueue<String> q = queueMap.getOrDefault(queue, new ArrayBlockingQueue<>(queueSize));
+		if (!queueMap.containsKey(queue)) {
+			queueMap.put(queue, q);
+		}
+		
+		q.offer(payload);
 	}
 
 	@Override
 	public Optional<String> poll(String queue) {
 		try {
-			return Optional.of(this.queue.take());
+			if (queueMap.containsKey(queue)) {
+				return Optional.of(queueMap.get(queue).take());
+			}
 		} catch (InterruptedException e) {
 			return Optional.empty();
 		}
+		
+		return Optional.empty();
 	}
 }
