8706d59ccb3b0cad02c0700ee26ec36a73669654
radjab <radjab@gmail.com>
2023-05-07 21:18:50 +0200

    dependency on ServiceConfig

--- src/main/java/io/linkedlogics/service/ConfigurableService.java
+++ src/main/java/io/linkedlogics/service/ConfigurableService.java
@@ -3,13 +3,14 @@ package io.linkedlogics.service;
 import java.lang.reflect.Proxy;
 
 import io.linkedlogics.service.config.ConfigInvocationHandler;
+import io.linkedlogics.service.config.ServiceConfig;
 import lombok.Getter;
 
-public abstract class ConfigurableService<T> {
+public abstract class ConfigurableService<T extends ServiceConfig> {
 	@Getter
 	private T config;
 	
-	public ConfigurableService(Class<T> configClass) {
+	public ConfigurableService(Class<? extends ServiceConfig> configClass) {
 		try {
 			config = (T) Proxy.newProxyInstance(configClass.getClassLoader(), new Class[] {configClass}, new ConfigInvocationHandler());
 		} catch (Exception e) {
